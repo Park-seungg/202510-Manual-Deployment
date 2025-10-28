c9945698c93d712a0a0ac7231f048f724e5a31c8
Park-seungg <parkis1101@gmail.com>
2025-10-28 10:53:50 +0900

    HomeController

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,4 +1,16 @@
 package com.example.demo;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return """
+                <h1>Welcome to the Home Page!</h1>
+                <p>This is a Demo Spring Boot application.</p>
+                """;
+    }
 }
